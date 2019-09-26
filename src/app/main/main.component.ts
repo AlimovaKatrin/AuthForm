c630c18d1e81d70e468c62365955fd90979d7bc2
AlimovaKatrin <alimova.katrin8@gmail.com>
2019-09-26 14:24:15 +0300

    main page component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-page',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
